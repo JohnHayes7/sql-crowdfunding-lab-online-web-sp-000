fb468f6d91aa4941f9bbb7b2fa87fb59fc6f120d
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 12:50:09 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -19,10 +19,11 @@ INSERT INTO users (name, age) VALUES ("Page", 57);
 INSERT INTO users (name, age) VALUES ("Jon", 57);
 INSERT INTO users (name, age) VALUES ("Mike", 57);
 
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Van Zaun Park Clean Up", "Community", 100, "Jan 1 2019", "Dec 31 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Van Zaun Park Clean Up", "Community", 100, "Jan 1 2019", "May 1 2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Festival", "Entertainment", 1000, "March 1 2019", "March 31 2020");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Album", "Music", 500, "Jan 1 2019", "July 31 2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Film", "Entertainment", 300, "Aug 23 2019", "Aug 23 2029");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Paint School", "Community", 1000, "Feb 1 2019", "Aug 1 2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Playground", "Community", 10000, "Jan 1 2019", "Dec 31 2019");
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Class Trip", "Education", 5000, "Feb 1 2019", "May 1 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("FDR Park Clean Up", "Community", 100, "Jan 1 2019", "May 1 2019");
