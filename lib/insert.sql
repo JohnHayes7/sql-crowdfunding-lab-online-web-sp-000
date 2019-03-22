a8da8e6a4712b73b97d1b5c5abc21d4ca5bc4e06
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 12:42:51 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO users (name, age) VALUES ("Johnny", 38);
+INSERT INTO users (name, age) VALUES ("Jen", 34);
+INSERT INTO users (name, age) VALUES ("Dee", 37);
+INSERT INTO users (name, age) VALUES ("Jon", 35);
+INSERT INTO users (name, age) VALUES ("Maureen", 35);
+INSERT INTO users (name, age) VALUES ("Matt", 34);
+INSERT INTO users (name, age) VALUES ("Wes", 39);
+INSERT INTO users (name, age) VALUES ("Ernie", 38);
+INSERT INTO users (name, age) VALUES ("Brendan", 36);
+INSERT INTO users (name, age) VALUES ("Patrick", 27);
+INSERT INTO users (name, age) VALUES ("Paul", 27);
+INSERT INTO users (name, age) VALUES ("Max", 50);
+INSERT INTO users (name, age) VALUES ("Mike", 22);
+INSERT INTO users (name, age) VALUES ("Ethan", 24);
+INSERT INTO users (name, age) VALUES ("Jill", 21);
+INSERT INTO users (name, age) VALUES ("Colin", 47);
+INSERT INTO users (name, age) VALUES ("Trey", 57);
+INSERT INTO users (name, age) VALUES ("Page", 57);
+INSERT INTO users (name, age) VALUES ("Jon", 57);
+INSERT INTO users (name, age) VALUES ("Mike", 57);
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Clean Up", "Community", 100, "Jan 1 2019", "Dec 31 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Festival", "Entertainment", 1000, "March 1 2019", "March 31 2020");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Album", "Music", 500, "Jan 1 2019", "July 31 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Film", "Entertainment", 300, "Aug 23 2019", "Aug 23 2029");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Paint School", "Community", 1000, "Feb 1 2019", "Aug 1 2019");
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("New Playground", "Community", 10000, "Jan 1 2019", "Dec 31 2019");
