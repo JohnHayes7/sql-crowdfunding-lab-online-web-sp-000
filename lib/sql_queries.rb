cb479f9fcffa06c787f9ceea8a99386d79fe1f84
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 13:46:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,10 @@ GROUP BY (users.name)"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT title, SUM(pledges.amount) FROM projects"
+"SELECT title, SUM(pledges.amount) FROM projects
+ JOIN pledges
+ ON projects.id = pledges.project_id
+ WHERE SUM(pledges.amount) > pledges.funding_gaol"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
