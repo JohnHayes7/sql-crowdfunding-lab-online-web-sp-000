b82db6bda49fae0db2cd26f12bbe6c7736fc4ced
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 13:30:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -16,7 +16,7 @@ end
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT name, SUM(pledges.amount) FROM users
 JOIN pledges
-ON user.id = pledges.project_id
+ON user.id = pledges.user_id
 GROUP BY (projects.title)"
 end
 
